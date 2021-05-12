d628317a9e4daecd1890e4411764bb4c6854233f
anton <707myemail@gmail.com>
2021-05-12 23:48:15 +0300

    slash put

--- src/test/java/com/pages/AllProductsPage.java
+++ src/test/java/com/pages/AllProductsPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.PageFactory;
 import java.util.List;
 
 public class AllProductsPage extends CommonMethods {
-
+//qqq
     @FindBy(xpath = "//h2[text() = 'All Products']")
     public WebElement allProductsSign;
 
