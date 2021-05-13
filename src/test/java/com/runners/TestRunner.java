baec31cd4e0e8ab3a18fd18050355602db8721e1
Petrov452 <petrov.andrey.chop@gmail.com>
2021-05-13 07:33:21 -0400

    my

--- src/test/java/com/runners/TestRunner.java
+++ src/test/java/com/runners/TestRunner.java
@@ -8,7 +8,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = "src/test/resources/features",
         glue = "com/stepdefinitions",
         dryRun = false,
-        tags = "@regression",
+        tags = "@addProducts",
         monochrome = false,
         plugin = {"pretty",
                 "html:target/cucumber-default-reports",
